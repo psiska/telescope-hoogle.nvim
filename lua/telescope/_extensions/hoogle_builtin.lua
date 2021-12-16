d20f1f1bd149b3c820fa3642d18b2d1f04b2d8ab
Peter Siska <siska.pe@gmail.com>
2021-12-16 10:43:24 +0100

    fix: By using standard finder since hoogle supports jsonl

--- lua/telescope/_extensions/hoogle_builtin.lua
+++ lua/telescope/_extensions/hoogle_builtin.lua
@@ -1,7 +1,8 @@
---local actions = require'telescope.actions'
+local actions = require'telescope.actions'
+local actions_state = require 'telescope.actions.state'
 local conf = require'telescope.config'.values
 local entry_display = require'telescope.pickers.entry_display'
-local mfinders = require'telescope.mfinders'
+local finders = require'telescope.finders'
 local log = require'telescope.log'
 local pickers = require'telescope.pickers'
 local previewers = require'telescope.previewers'
@@ -110,8 +111,6 @@ end
  .type -> String
  .url -> String -- https://hackage.haskell.org/package/base/docs/Prelude.html#v:map
 ]]
--- TODO move to some text utils
-
 
 local function gen_from_hoogle(_)
   local displayer = entry_display.create{
@@ -126,49 +125,82 @@ local function gen_from_hoogle(_)
   local function make_display(entry)
     --log.debug("make_display:entry: "..vim.inspect(entry))
     return displayer{
-      {entry.module, 'HaskellModule'},
+      {entry.module_name, 'HaskellModule'},
       {entry.package, 'HaskellPackage'},
       entry.ordinal,
     }
   end
 
   return function(line)
-    local result = {}
-    local hoogleJson = json.decode(line)
-    for _, v in ipairs(hoogleJson) do
-      local obj = {
-        value = v['docs'] or "",
-        module = v['module']['name'] or "",
-        package = v['package']['name'],
-        ordinal = v['item'],
-        display = make_display,
-        preview_command = previewEntry
-      }
-      table.insert(result, obj)
-    end
-    return result
+    local v = json.decode(line)
+    local obj = {
+      value = v['docs'] or "",
+      module_name = v['module']['name'] or "",
+      type_sig = v['item'],
+      package = v['package']['name'],
+      ordinal = v['item'],
+      display = make_display,
+      preview_command = previewEntry
+    }
+    return obj
   end
+end
+
+local function copy_to_clipboard(text)
+  local reg = vim.o.clipboard == 'unnamedplus' and '+' or '"'
+  vim.fn.setreg(reg, text)
+end
+
+local function open_browser(url)
+  local browser_cmd
+  if vim.fn.has('unix') == 1 then
+    browser_cmd = 'sensible-browser'
+  end
+  if vim.fn.has('mac') == 1 then
+    browser_cmd = 'open'
+  end
+
+  vim.cmd(':silent !' .. browser_cmd .. ' ' .. vim.fn.fnameescape(url))
+end
+
+
+local function prompt_fn(opts)
+  local function hoogle_cmd(prompt)
+    if not prompt or string.len(prompt) < 2 then
+      return nil
+    end
+    return { opts.bin, '--jsonl', '--count=20', '-q',  prompt}
+  end
+
+  return hoogle_cmd
 end
 
 M.list = function(opts)
   opts = opts or {}
-  opts.bin = opts.bin and vim.fn.expand(opts.bin) or vim.fn.exepath('hoogle')
   opts.cwd = utils.get_lazy_default(opts.cwd, vim.fn.getcwd)
-  opts.entries_maker = utils.get_lazy_default(opts.entries_maker, gen_from_hoogle, opts)
+  opts.entry_maker = utils.get_lazy_default(opts.entry_maker, gen_from_hoogle, opts)
+  opts.bin = opts.bin and vim.fn.expand(opts.bin) or vim.fn.exepath('hoogle')
 
   --A Sorter is called by the Picker on each item returned by the Finder. It return a number, which is equivalent to the "distance" between the current prompt and the entry returned by a finder.
   pickers.new(opts, {
     prompt_title = 'Hoogle query',
-    finder = mfinders.new_multi_entries_job(function(prompt)
-      if not prompt or string.len(prompt) < 3 then
-        return nil
-      end
-      return { opts.bin, '--json', '--count=10', prompt}
-      end,
-      opts.entries_maker
-      ),
+    finder = finders.new_job(prompt_fn(opts), opts.entry_maker, 20, opts.cwd),
     sorter = conf.file_sorter(opts),
     previewer = previewers.display_content.new(opts),
+    attach_mappings = function(buf, map)
+      map('i', '<CR>', function()
+        local entry = actions_state.get_selected_entry()
+        copy_to_clipboard(entry.type_sig)
+        actions.close(buf)
+      end)
+      map('i', '<C-b>', function()
+        local entry = actions_state.get_selected_entry()
+        open_browser(entry.url)
+        actions.close(buf)
+      end)
+
+      return true
+    end
   }):find()
 end
 
